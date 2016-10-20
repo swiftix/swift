3d96856de2a75afae1c84b8c978318e9168fb49a
Roman Levenstein <rlevenstein@apple.com>
2016-10-20 12:38:00 -0700

    [sil-performance-inliner] Use the profitability check in complex cases to decide if a generic function should be inlined
    Doing so protects from unbound code growth among other things. The profitability computation heuristic will be extended in the subsequent commits to better support generic functions.

--- lib/SILOptimizer/Transforms/PerformanceInliner.cpp
+++ lib/SILOptimizer/Transforms/PerformanceInliner.cpp
@@ -489,7 +489,8 @@ bool SILPerformanceInliner::isProfitableToInlineGeneric(FullApplySite AI,
   SILFunction *Callee = AI.getReferencedFunction();
 
   if (EnableSILInliningOfGenerics) {
-    return true;
+    return isProfitableToInline(AI, CallerWeight, callerTracker, NumCallerBlocks,
+                              /* IsGeneric */ true);
   }
 
   return false;
