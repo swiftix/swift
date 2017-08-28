162a1351006e6e540e0787f046183afcf6302bec
Roman Levenstein <rlevenstein@apple.com>
2017-08-28 13:03:33 -0700

    Minor dead function elimination clean-up.

--- lib/SILOptimizer/IPO/DeadFunctionElimination.cpp
+++ lib/SILOptimizer/IPO/DeadFunctionElimination.cpp
@@ -31,6 +31,14 @@ STATISTIC(NumEliminatedExternalDefs, "Number of external function definitions el
 
 namespace {
 
+/// Returns true if a function should be SIL serialized or emitted by IRGen.
+static bool shouldBeSerializedOrEmitted(SILFunction *F) {
+  if (F->isAvailableExternally() && hasPublicVisibility(F->getLinkage()) &&
+      !F->isTransparent())
+    return false;
+  return true;
+}
+
 /// This is a base class for passes that are based on function liveness
 /// computations like e.g. dead function elimination.
 /// It provides a common logic for computing live (i.e. reachable) functions.
@@ -112,6 +120,11 @@ protected:
     if (isPossiblyUsedExternally(F->getLinkage(), Module->isWholeModule()))
       return true;
 
+    // Do not consider public_external functions that do not need to be emitted
+    // into the client as anchors.
+    if (!shouldBeSerializedOrEmitted(F))
+      return false;
+
     // ObjC functions are called through the runtime and are therefore alive
     // even if not referenced inside SIL.
     if (F->getRepresentation() == SILFunctionTypeRepresentation::ObjCMethod)
@@ -132,9 +145,7 @@ protected:
       return true;
     }
 
-    // stdlib_binary_only functions should never be removed as they
-    // may be referenced externally.
-    if (F->hasSemanticsAttr("stdlib_binary_only"))
+    if (F->isVersioned())
       return true;
 
     return false;
@@ -358,6 +369,10 @@ protected:
 
   /// Scans all references inside a function.
   void scanFunction(SILFunction *F) {
+    // Don't scan functions that are not going to be SIL serialized or emitted
+    // by IRGen, because they do not keep anything alive.
+    if (!shouldBeSerializedOrEmitted(F))
+      return;
 
     DEBUG(llvm::dbgs() << "    scan function " << F->getName() << '\n');
 
